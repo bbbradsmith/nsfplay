89010dbe1fa064a43add02408b188674d7fdbb99
rainwarrior@gmail.com <rainwarrior@gmail.com@98339eb9-417f-3607-c4bb-4869709a757b>
2012-08-17 09:26:53 +0000

    FDS bankswitch mirrored-write workaround

--- trunk/xgm/devices/Memory/nes_bank.cpp
+++ trunk/xgm/devices/Memory/nes_bank.cpp
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include "nes_bank.h"
 
+// this workaround solves a problem with mirrored FDS RAM writes
+// when the same bank is used twice; some NSF rips reuse bank 00
+// in "unused" banks that actually get written to.
+//
+// eventually want to fix the NSFs and undo this solution
+#define FDS_MEMCPY 1
+
+#if FDS_MEMCPY
+static UINT8* fds_image = NULL;
+#endif
+
 namespace xgm
 {
 
@@ -15,6 +26,11 @@ namespace xgm
   {
     if (image)
       delete[]image;
+
+    #if FDS_MEMCPY
+    if (fds_image)
+      delete[] fds_image;
+    #endif
   }
 
   void NES_BANK::SetBankDefault (UINT8 bank, int value)
@@ -28,7 +44,7 @@ namespace xgm
 
     // バンクスイッチの初期値は全て「バンク無効」
     for (i = 0; i < 16; i++)
-      bankdefault[i] = -1;
+      bankdefault[i] = -1; // -1 is special empty bank
 
     bankmax = (((offset & 0xfff) + size) / 0x1000) + 1;
     if (bankmax > 256)
@@ -40,10 +56,19 @@ namespace xgm
     memset (image, 0, 0x1000 * bankmax);
     memcpy (image + (offset & 0xfff), data, size);
 
+    #if FDS_MEMCPY
+      if (fds_image)
+        delete[] fds_image;
+      fds_image = new UINT8[0x10000];
+      memset(fds_image, 0, 0x10000);
+      for (i = 0; i < 16; i++)
+        bank[i] = fds_image + 0x1000 * i;
+    #else
     for (i = 0; i < bankmax; i++)
       bank[i] = image + 0x1000 * i;
     for (i = bankmax; i < 256; i++)
       bank[i] = null_bank;
+    #endif
 
     return true;
   }
@@ -52,11 +77,25 @@ namespace xgm
   {
     memset (null_bank, 0, 0x1000);
     for (int i = 0; i < 16; i++)
+    {
       bankswitch[i] = bankdefault[i];
+
+      #if FDS_MEMCPY
+        bankswitch[i] = i;
+        if (bankdefault[i] == -1 || bankdefault[i] >= bankmax)
+          memset(bank[i], 0, 0x1000);
+        else
+          memcpy(bank[i], image + (bankdefault[i] * 0x1000), 0x1000);
+      #endif
+    }
   }
 
   bool NES_BANK::Write (UINT32 adr, UINT32 val, UINT32 id)
   {
+    #if FDS_MEMCPY
+    if (!fds_enable)
+    #endif
+
     if (0x5ff8 <= adr && adr < 0x6000)
     {
       bankswitch[(adr & 7) + 8] = val & 0xff;
@@ -65,14 +104,38 @@ namespace xgm
 
     if (fds_enable)
     {
+      #if FDS_MEMCPY
+      if (0x5ff6 <= adr && adr < 0x6000)
+      {
+        int b = adr - 0x5ff0;
+        if (int(val) >= bankmax)
+          memset(bank[b], 0, 0x1000);
+        else
+          memcpy(bank[b], image + (val * 0x1000), 0x1000);
+        return true;
+      }
+      #else
       if (0x5ff6 <= adr && adr < 0x5ff8)
       {
         bankswitch[adr & 7] = val & 0xff;
         return true;
       }
+      #endif
 
       if (0 <= bankswitch[adr >> 12] && 0x6000 <= adr && adr < 0xe000)
       {
+        // for detecting FDS ROMs with improper mirrored writes
+        //
+        //for (int i=0; i < 14; ++i)
+        //{
+        //  int b = adr >> 12;
+        //  if (i != b && bankswitch[i] == bankswitch[b])
+        //  {
+        //    DEBUG_OUT("[%04X] write mirrored to [%04X] = %02X\n",
+        //      adr, (i * 0x1000) | (adr & 0x0fff), val);
+        //  }
+        // }
+
         bank[bankswitch[adr >> 12]][adr & 0x0fff] = val;
         return true;
       }
