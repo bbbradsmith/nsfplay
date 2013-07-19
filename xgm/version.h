a610766cd2d62a95d0ff7a8132cf9bfcd748faf9
rainwarrior <rainwarrior@gmail.com>
2013-07-19 02:18:34 +0000

    marking 2.3 beta 5

--- xgm/version.h
+++ xgm/version.h
@@ -1,5 +1,5 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
-#define NSFPLAY_TITLE "NSFPlay 2.3 beta 4"
-#define NSFPLAY_VERSION "2.3 beta 4"
+#define NSFPLAY_TITLE "NSFPlay 2.3 beta 5"
+#define NSFPLAY_VERSION "2.3 beta 5"
 #endif
