6bf53178e56e104f2bb22178b035de772df22b83
rainwarrior <rainwarrior@98339eb9-417f-3607-c4bb-4869709a757b>
2014-04-22 06:35:28 +0000

    fixing incorrect bank count (crash bug on 1MB NSFs)

--- setup.bat
+++ setup.bat
@@ -1,6 +1,10 @@
 @echo This batch file sets up in_yansf.ini for debugging.
 md Debug
+md Debug\plugins
+cd ..
 md Release
+md Release\plugins
+cd ..
 copy distribute\plugins\in_yansf.ini Debug\plugins\in_yansf.ini
 copy distribute\plugins\in_yansf.ini Release\plugins\in_yansf.ini
 pause
