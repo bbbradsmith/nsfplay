da19dc01722585c3d4b0e4e3e3d6c36cc4be32e0
bbbradsmith <bbbradsmith@users.noreply.github.com>
2019-03-04 04:46:19 -0500

    preparing beta 10

--- xgm/version.h
+++ xgm/version.h
@@ -1,5 +1,5 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
-#define NSFPLAY_TITLE "NSFPlay 2.4 beta (unofficial)"
-#define NSFPLAY_VERSION "2.4 beta (unofficial)"
+#define NSFPLAY_TITLE "NSFPlay 2.4 beta 10"
+#define NSFPLAY_VERSION "2.4 beta 10"
 #endif
