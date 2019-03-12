6995c8a6b85ffb187e48e65cc8aba53b31ed9cc1
bbbradsmith <bbbradsmith@users.noreply.github.com>
2019-03-12 04:51:55 -0400

    marking unofficial builds

--- xgm/version.h
+++ xgm/version.h
@@ -1,5 +1,5 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
-#define NSFPLAY_TITLE "NSFPlay 2.4 beta 13"
-#define NSFPLAY_VERSION "2.4 beta 13"
+#define NSFPLAY_TITLE "NSFPlay 2.4 beta 14 (unofficial)"
+#define NSFPLAY_VERSION "2.4 beta 14 (unofficial)"
 #endif
