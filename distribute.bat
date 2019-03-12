869c240dac4e581eff0d7694be5abd6fa7b10d82
bbbradsmith <bbbradsmith@users.noreply.github.com>
2019-03-12 04:43:12 -0400

    moving distrbute.bat / push_winamp.bat out of distribute folder (will adjust internal paths momentarily)

---
+++ distribute.bat
@@ -0,0 +1,4 @@
+copy ..\Release\nsfplay.exe nsfplay.exe
+copy ..\Release\plugins\in_yansf.dll plugins\in_yansf.dll
+copy ..\Release\plugins\nsfplug_ui.dll plugins\nsfplug_ui.dll
+@pause
--- distribute/push_winamp.bat
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0"
-call distribute.bat
-copy plugins\*.* "C:\Program Files (x86)\Winamp\Plugins"
-pause
