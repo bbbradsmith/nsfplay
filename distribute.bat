72819a18f4d06e075ba5196021bc624916e2c25a
bbbradsmith <bbbradsmith@users.noreply.github.com>
2019-04-02 01:37:08 -0400

    adding mkdir to distribut.bat for automated builds

--- distribute.bat
+++ distribute.bat
@@ -1,4 +1,5 @@
 copy Release\nsfplay.exe distribute\nsfplay.exe
+mkdir distribute\plugins
 copy Release\plugins\in_yansf.dll distribute\plugins\in_yansf.dll
 copy Release\plugins\nsfplug_ui.dll distribute\plugins\nsfplug_ui.dll
 del distribute\plugins\in_yansf.ini
