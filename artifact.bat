418d09f8bcbbf49a80490618cc5642fe67454b48
bbbradsmith <bbbradsmith@users.noreply.github.com>
2019-04-02 01:44:08 -0400

    artifact.bat distribute script with no pause for automated builds

---
+++ artifact.bat
@@ -0,0 +1,7 @@
+mkdir artifact
+mkdir artifact\plugins
+copy Release\nsfplay.exe artifact\nsfplay.exe
+copy distribute\nsfplay.txt artifact\nsfplay.txt
+copy distribute\nsfplay.ini artifact\nsfplay.ini
+copy Release\plugins\in_yansf.dll artifact\plugins\in_yansf.dll
+copy Release\plugins\nsfplug_ui.dll artifact\plugins\nsfplug_ui.dll
