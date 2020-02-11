ffd144530156b738f87a6d042e47d63ec94dafde
John Regan <john@jrjrtech.com>
2020-02-10 20:13:04 -0500

    xgm: remove set but unused variable

--- xgm/player/nsf/pls/ppls.cpp
+++ xgm/player/nsf/pls/ppls.cpp
@@ -329,11 +329,10 @@ void PLSITEM_delete(PLSITEM *elem)
 static const char *print_time(int time)
 {
   static char buf[32] ;
-  int h,m,s,ss = 0 ;
+  int h,m,s = 0 ;
 
   if(time<0) return "" ;
 
-  ss = (time%1000) / 10 ;
   time /= 1000 ;
   s = time%60 ;
   time /= 60 ;
