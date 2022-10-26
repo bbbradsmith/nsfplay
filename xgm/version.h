fa9a2c0f8bb8739fe5dd620c11be42ed6d296d37
bbbradsmith <bbbradsmith@users.noreply.github.com>
2022-10-25 21:43:52 -0400

    no longer beta

--- xgm/version.h
+++ xgm/version.h
@@ -1,5 +1,5 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
-#define NSFPLAY_TITLE "NSFPlay 2.5 beta (unofficial)"
-#define NSFPLAY_VERSION "2.5 beta (unofficial)"
+#define NSFPLAY_TITLE "NSFPlay 2.5"
+#define NSFPLAY_VERSION "2.5"
 #endif
