e22e5143aa01d525860d4f74ccec5b215f3a2780
bbbradsmith <bbbradsmith@users.noreply.github.com>
2023-07-03 22:49:09 -0400

    2.6 release

--- xgm/version.h
+++ xgm/version.h
@@ -1,6 +1,6 @@
 #ifndef _XGM_VERSION_H_
 #define _XGM_VERSION_H_
-#define NSFPLAY_TITLE "NSFPlay 2.6 beta (unofficial)"
-#define NSFPLUG_TITLE "NSFPlug 2.6 beta (unofficial)"
-#define NSFPLAY_VERSION "2.6 beta (unofficial)"
+#define NSFPLAY_TITLE "NSFPlay 2.6"
+#define NSFPLUG_TITLE "NSFPlug 2.6"
+#define NSFPLAY_VERSION "2.6"
 #endif
